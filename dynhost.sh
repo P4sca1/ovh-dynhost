4b5b1508b32a820c74d4bf7989775836df2863bf
Pascal Sthamer <sthamer.pascal@gmail.com>
2019-06-30 12:19:38 +0200

    Fix output redirection
    The syntax we used is only valid in bash.

--- dynhost.sh
+++ dynhost.sh
@@ -8,7 +8,7 @@ CURRENT_IP=$(dig +short $DYNHOST_DOMAIN_NAME)
 if [ "$IP" ]; then 
     if [ "$CURRENT_IP" != "$IP" ]; then 
         echo "Updating $DYNHOST_DOMAIN_NAME from $CURRENT_IP to $IP" >> dynhost.log
-        curl --user "${DYNHOST_LOGIN}:${DYNHOST_PASSWORD}" "http://www.ovh.com/nic/update?system=dyndns&hostname=${DYNHOST_DOMAIN_NAME}&myip=${IP}" &>> dynhost.log
+        curl --user "${DYNHOST_LOGIN}:${DYNHOST_PASSWORD}" "http://www.ovh.com/nic/update?system=dyndns&hostname=${DYNHOST_DOMAIN_NAME}&myip=${IP}" >> dynhost.log 2>&1
     else 
         echo "No update required" >> dynhost.log
     fi 
